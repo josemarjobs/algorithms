79dda6c45703aaff51dd51385ebf03419caa7288
Josemar Magalhaes <josemarmagalhaes@hotmail.com>
2016-03-21 00:21:27 -0700

    Greatest common divisor naive solution

---
+++ greatest_common_divisor/naive-solution.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"log"
+	"os"
+)
+
+func gcd(a, b int) (best int) {
+	// just to be safe
+	if a > 10000000 || b > 10000000 {
+		log.Println("Numbers too big")
+		os.Exit(1)
+	}
+
+	for i := 1; i <= (a + b); i++ {
+		if a%i == 0 && b%i == 0 {
+			best = i
+		}
+	}
+	return
+}
+
+func main() {
+	log.Println(gcd(100, 4))
+	log.Println(gcd(134352, 41223))
+}
