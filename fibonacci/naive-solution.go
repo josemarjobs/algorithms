6509f6ec03dfc028638ce95c2275ed4faff52ae7
Josemar Magalhaes <josemarmagalhaes@hotmail.com>
2016-03-21 00:20:44 -0700

    Fibonacci naive solution

---
+++ fibonacci/naive-solution.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"log"
+	"os"
+)
+
+// do not run this on numbers bigger the 60
+func fib(n int) int {
+	if n > 50 {
+		log.Println("Number too big")
+		os.Exit(1)
+	}
+	if n <= 1 {
+		return n
+	} else {
+		return fib(n-1) + fib(n-2)
+	}
+}
+func main() {
+	for i := 0; i < 20; i++ {
+		log.Println(i, ":", fib(i), " - ", T(i))
+	}
+}
+
+func T(n int) int {
+	if n <= 1 {
+		return 2
+	} else {
+		return (T(n-1) + T(n-2)) + 3
+	}
+}
+
+// 0 1 1 2 3 5 8 13
