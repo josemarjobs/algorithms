1259b0af71c9142ca75cd1318e2ba6386c9b572c
Josemar Magalhaes <josemarmagalhaes@hotmail.com>
2016-03-21 00:21:00 -0700

    Fibonacci efficient solution

---
+++ fibonacci/efficient-solution.go
@@ -0,0 +1,22 @@
+package main
+
+import "log"
+
+func fib(n int) int {
+	f := []int{0, 1}
+	for i := 2; i <= n; i++ {
+		f = append(f, f[i-1]+f[i-2])
+	}
+	return f[n]
+}
+func main() {
+	for i := 0; i < 50; i++ {
+		log.Println(i, ":", fib(i), " - ", T(i))
+	}
+}
+
+func T(n int) int {
+	return 2*n + 2
+}
+
+// 0 1 1 2 3 5 8 13
